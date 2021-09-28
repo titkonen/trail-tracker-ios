26eea8a7105c02a5522a17ea625dfa7a7dde7518
Toni Itkonen <toni.itkonen@icloud.com>
2021-09-28 15:09:57 +0300

    Start and stopLocationManager functions, GetSpot button moved to bottom area. Error handling codes added.

--- Trailhero/View Controllers/SpotsViewController.swift
+++ Trailhero/View Controllers/SpotsViewController.swift
@@ -4,24 +4,29 @@ import CoreLocation
 class SpotsViewController: UIViewController, CLLocationManagerDelegate {
   
   // MARK: OUTLETS
-  @IBOutlet weak var spotsButton: UIBarButtonItem!
+  @IBOutlet weak var getSpot: UIButton!
   @IBOutlet weak var latitudeLabel: UILabel!
   @IBOutlet weak var longitudeLabel: UILabel!
+  @IBOutlet weak var messageLabel: UILabel!
   
   // MARK: PROPERTIES
   var location: CLLocation? ///Storing the location
   let locationManager = CLLocationManager()
+  var updatingLocation = false
+  var lastLocationError: Error?
   
   // MARK: VIEW LIFE CYCLE
   override func viewDidLoad() {
     super.viewDidLoad()
     
     //view.backgroundColor = .yellow
+    updateLabels()
   }
   
   // MARK: ACTIONS
-  @IBAction func addSpotsButtonPressed(_ sender: UIBarButtonItem) {
-    print("Spot button pressed")
+  
+  @IBAction func getSpotPressed(_ sender: UIButton) {
+    print("Get button pressed")
     let authStatus = locationManager.authorizationStatus
     if authStatus == .notDetermined {
       locationManager.requestWhenInUseAuthorization()
@@ -33,10 +38,16 @@ class SpotsViewController: UIViewController, CLLocationManagerDelegate {
       return
     }
     
-    locationManager.delegate = self
-    locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-    locationManager.startUpdatingLocation()
+    if updatingLocation {
+      stopLocationManager()
+    } else {
+      location = nil
+      lastLocationError = nil
+      startLocationManager()
+    }
+    updateLabels()
   }
+  
   
   // MARK: FUNCTIONS
   func updateLabels() {
@@ -48,27 +59,91 @@ class SpotsViewController: UIViewController, CLLocationManagerDelegate {
         format: "%.8f",
         location.coordinate.longitude)
       //tagButton.isHidden = false
-      //messageLabel.text = ""
+      messageLabel.text = ""
     } else {
       latitudeLabel.text = ""
       longitudeLabel.text = ""
       //addressLabel.text = ""
       //tagButton.isHidden = true
-      //messageLabel.text = "Tap 'Get My Location' to Start"
+      let statusMessage: String
+          if let error = lastLocationError as NSError? {
+            if error.domain == kCLErrorDomain && error.code == CLError.denied.rawValue {
+              statusMessage = "Location Services Disabled"
+            } else {
+              statusMessage = "Error Getting Location"
+            }
+          } else if !CLLocationManager.locationServicesEnabled() {
+            statusMessage = "Location Services Disabled"
+          } else if updatingLocation {
+            statusMessage = "Searching..."
+          } else {
+            statusMessage = "Tap 'Get My Location' to Start"
+          }
+          messageLabel.text = statusMessage
+    }
+    configureGetButton()
+  }
+  
+  func configureGetButton() {
+    if updatingLocation {
+      getSpot.setTitle("Stop", for: .normal)
+    } else {
+      getSpot.setTitle("Get My Location", for: .normal)
+    }
+  }
+  
+  func startLocationManager() {
+    if CLLocationManager.locationServicesEnabled() {
+      locationManager.delegate = self
+      locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+      locationManager.startUpdatingLocation()
+      updatingLocation = true
+    }
+  }
+  
+  func stopLocationManager() {
+    if updatingLocation {
+      locationManager.stopUpdatingLocation()
+      locationManager.delegate = nil
+      updatingLocation = false
     }
   }
   
   // MARK: LOCATION MANAGER FUNCTIONS
   func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
     print("didFailWithError \(error.localizedDescription)")
+    
+    if (error as NSError).code == CLError.locationUnknown.rawValue {
+        return
+    }
+      lastLocationError = error
+      stopLocationManager()
+      updateLabels()
   }
   
   func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
       let newLocation = locations.last!
       print("didUpdateLocations \(newLocation)")
-      
-      location = newLocation
-      updateLabels()
+
+      if newLocation.timestamp.timeIntervalSinceNow < -5 {
+        return
+      }
+
+      if newLocation.horizontalAccuracy < 0 {
+        return
+      }
+
+      if location == nil || location!.horizontalAccuracy > newLocation.horizontalAccuracy {
+
+        lastLocationError = nil
+        location = newLocation
+
+        if newLocation.horizontalAccuracy <= locationManager.desiredAccuracy {
+          print("*** We're done!")
+          stopLocationManager()
+        }
+        updateLabels()
+      }
   }
   
   // MARK: - Helper Methods
@@ -88,5 +163,4 @@ class SpotsViewController: UIViewController, CLLocationManagerDelegate {
   }
   
 }
-
 
