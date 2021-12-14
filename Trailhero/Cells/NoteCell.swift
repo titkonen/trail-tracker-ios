4dca8be2db0916733d468ac45403b5862bd6bc2a
Toni Itkonen <toni.itkonen@icloud.com>
2021-12-14 08:24:23 +0200

    Different kind of testing for fetching map route, but not yet success. Will switch the branch.

--- Trailhero/Cells/NoteCell.swift
+++ Trailhero/Cells/NoteCell.swift
@@ -1,19 +1,57 @@
 import UIKit
+import CoreLocation
+import MapKit
 
 class NoteCell: UITableViewCell {
-    
-    var noteData: Run! {
+  
+  // MARK: PROPERTIES
+  private var run: Run?
+  private let locationManager = LocationManager.shared
+  private var locationList: [CLLocation] = []
+  private var distance = Measurement(value: 0, unit: UnitLength.meters)
+  
+  // MARK: DIDSETS
+  var noteData: Run! {
         didSet {
-            
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "MM/dd/yy hh:mm"
-            noteTitle.text = String(format: "%.1f",noteData.distance) + " m"
-            previewLabel.text = String(noteData.duration) + " sec"
-            dateLabel.text = dateFormatter.string(from: noteData.timestamp ?? Date())
-            
+          let dateFormatter = DateFormatter()
+          dateFormatter.dateFormat = "MM/dd/yy hh:mm"
+          dateLabel.text = dateFormatter.string(from: noteData.timestamp ?? Date())
+          noteTitle.text = String(format: "%.1f",noteData.distance) + " m"
+          previewLabel.text = String(noteData.duration) + " sec"
         }
+  }
+  
+  var paikkaTieto: Location! {
+    didSet {
+      let dateFormatter = DateFormatter()
+      dateFormatter.dateFormat = "MM/dd/yy hh:mm"
+      dateLabelLocation.text = dateFormatter.string(from: paikkaTieto.timestamp ?? Date())
+      //print(paikkaTieto)
+      latitudeLabel.text = String(paikkaTieto.latitude) + "lat..."
     }
+  }
     
+  //MARK: Location UI Properties
+  /// Date label location
+  fileprivate var dateLabelLocation: UILabel = {
+      let label = UILabel()
+      label.text = "Location dateLabel placeholder"
+      label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+      label.textColor = UIColor.white.withAlphaComponent(0.5)
+      return label
+  }()
+  
+  /// Note title
+  fileprivate var latitudeLabel: UILabel = {
+      let label = UILabel()
+      label.text = "Lat. placeholder"
+      label.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+      label.textColor = .white
+      return label
+  }()
+
+
+  //MARK: Tracking UI Properties
     /// Note title
     fileprivate var noteTitle: UILabel = {
         let label = UILabel()
@@ -31,7 +69,7 @@ class NoteCell: UITableViewCell {
         label.textColor = UIColor.white.withAlphaComponent(0.5)
         return label
     }()
-        
+  
     /// Preview label
     fileprivate var previewLabel: UILabel = {
         let label = UILabel()
@@ -44,7 +82,7 @@ class NoteCell: UITableViewCell {
     
     /// horizontal stack view
     fileprivate lazy var horizontalStackView: UIStackView = {
-        let s = UIStackView(arrangedSubviews: [noteTitle, previewLabel, UIView()])
+        let s = UIStackView(arrangedSubviews: [noteTitle, previewLabel, latitudeLabel, UIView()])
         s.axis = .horizontal
         s.spacing = 20
         s.alignment = .leading
@@ -53,31 +91,60 @@ class NoteCell: UITableViewCell {
     
     /// vertical stack view
     fileprivate lazy var verticalStackView: UIStackView = {
-        let s = UIStackView(arrangedSubviews: [dateLabel, horizontalStackView])
+        let s = UIStackView(arrangedSubviews: [dateLabel, dateLabelLocation, horizontalStackView])
         s.axis = .vertical
         s.spacing = 5
-        
         s.translatesAutoresizingMaskIntoConstraints = false
         return s
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-    
         contentView.backgroundColor = UIColor(red: 2/255, green: 89/255, blue: 89/255, alpha: 1)
-      
         contentView.addSubview(verticalStackView)
         verticalStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20).isActive = true
         verticalStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
         verticalStackView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
-        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-     
 }
 
 
+// MARK: - Location Manager Delegate
+extension NoteCell: CLLocationManagerDelegate {
+  
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    for newLocation in locations {
+      let howRecent = newLocation.timestamp.timeIntervalSinceNow
+      guard newLocation.horizontalAccuracy < 20 && abs(howRecent) < 10 else { continue }
+      
+      if let lastLocation = locationList.last {
+        let delta = newLocation.distance(from: lastLocation)
+        distance = distance + Measurement(value: delta, unit: UnitLength.meters)
+        let coordinates = [lastLocation.coordinate, newLocation.coordinate]
+        //mapView.addOverlay(MKPolyline(coordinates: coordinates, count: 2))
+        let region = MKCoordinateRegion.init(center: newLocation.coordinate, latitudinalMeters: 500, longitudinalMeters: 500)
+        //mapView.setRegion(region, animated: true)
+      }
+      
+      locationList.append(newLocation)
+    }
+  }
+}
+
+// MARK: - Map View Delegate
+extension NoteCell: MKMapViewDelegate {
+  func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+    guard let polyline = overlay as? MKPolyline else {
+      return MKOverlayRenderer(overlay: overlay)
+    }
+    let renderer = MKPolylineRenderer(polyline: polyline)
+    renderer.strokeColor = .blue
+    renderer.lineWidth = 3
+    return renderer
+  }
+}
--- Trailhero/View Controllers/SavedTrailsDetailsVC.swift
+++ Trailhero/View Controllers/SavedTrailsDetailsVC.swift
@@ -1,15 +1,24 @@
 import UIKit
 import MapKit
+import CoreLocation
+import CoreData
 
 class SavedTrailsDetailsVC: UIViewController, UINavigationControllerDelegate {
   
   // MARK: Outlets
   @IBOutlet weak var mapView: MKMapView!
+
+  // MARK: TESTING
+  private var paikat: Location!
   
   // MARK: Properties
   //var run = [Run]()
-  var run: Run!
-
+  //var run: Run!
+  private var run: Run!
+  private let locationManager = LocationManager.shared
+  private var locationList: [CLLocation] = []
+  let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext // Context Layer
+  
   let dateFormatter: DateFormatter = {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "MMMM dd, YYYY hh:mm"
@@ -25,13 +34,43 @@ class SavedTrailsDetailsVC: UIViewController, UINavigationControllerDelegate {
       }
   }
   
+  var paikkaTieto: Location! {
+    didSet {
+      dateLabelLocation.text = dateFormatter.string(from: paikkaTieto.timestamp ?? Date())
+      latitudeLabel.text = String(paikkaTieto.latitude) + "lat..."
+    }
+  }
+  
+  //MARK: Location UI Properties
+  fileprivate lazy var dateLabelLocation: UILabel = {
+      let label = UILabel()
+      label.translatesAutoresizingMaskIntoConstraints = false
+      label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+      label.textColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+      label.text = dateFormatter.string(from: noteData.timestamp ?? Date())
+      //label.text = "HELLO dateLabel 3"
+      label.textAlignment = .left
+      return label
+  }()
+  
+  fileprivate lazy var latitudeLabel: UILabel = {
+      let label = UILabel()
+      label.translatesAutoresizingMaskIntoConstraints = false
+      label.font = UIFont.systemFont(ofSize: 40, weight: .semibold)
+      label.textColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+      label.text = "latitudeLabel placeholder"
+      label.textAlignment = .right
+      return label
+  }()
+  
+  //MARK: Tracking UI Properties
   fileprivate lazy var dateLabel: UILabel = {
       let label = UILabel()
       label.translatesAutoresizingMaskIntoConstraints = false
       label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
       label.textColor = UIColor(red: 242/255, green: 224/255, blue: 201/255, alpha: 1)
       label.text = dateFormatter.string(from: noteData.timestamp ?? Date())
-      //label.text = "HELLO HELLO 3"
+      //label.text = "HELLO dateLabel 3"
       label.textAlignment = .left
       return label
   }()
@@ -42,7 +81,7 @@ class SavedTrailsDetailsVC: UIViewController, UINavigationControllerDelegate {
       label.font = UIFont.systemFont(ofSize: 40, weight: .semibold)
       label.textColor = UIColor(red: 242/255, green: 224/255, blue: 201/255, alpha: 1)
       //label.text = dateFormatter.string(from: Date())
-      label.text = "distance !!!"
+      label.text = "distanceLabel !!!"
       label.textAlignment = .left
       return label
   }()
@@ -53,13 +92,13 @@ class SavedTrailsDetailsVC: UIViewController, UINavigationControllerDelegate {
       label.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
       label.textColor = UIColor(red: 242/255, green: 224/255, blue: 201/255, alpha: 1)
       //label.text = dateFormatter.string(from: Date())
-      label.text = "distance !!!"
+      label.text = "durationLabel !!!"
       label.textAlignment = .right
       return label
   }()
 
-  /*
-  fileprivate lazy var mapView: MKMapView = {
+  
+  /*fileprivate lazy var mapView: MKMapView = {
       let kartta = MKMapView()
       
       let leftMargin:CGFloat = 0
@@ -80,7 +119,14 @@ class SavedTrailsDetailsVC: UIViewController, UINavigationControllerDelegate {
     super.viewDidLoad()
     print("viewDidLoad is loaded")
     setupUI()
+    //loadLocations()
+    lataaPaikkaObjekti()
     loadMap()
+    
+    print(noteData.distance)
+//    print(paikkaTieto.latitude)
+    print(noteData.timestamp)
+    //print(paikkaTieto.timestamp ?? "test")
   }
   
   override func viewWillAppear(_ animated: Bool) {
@@ -154,6 +200,29 @@ class SavedTrailsDetailsVC: UIViewController, UINavigationControllerDelegate {
     return segments
   }
   
+  /// Fetch locations from coredata
+  /*private func loadLocations() -> [Run] {
+      let fetchRequest: NSFetchRequest<Run> = Run.fetchRequest()
+      let sortDescriptor = NSSortDescriptor(key: #keyPath(Run.timestamp), ascending: true)
+      fetchRequest.sortDescriptors = [sortDescriptor]
+      do {
+        return try CoreDataStack.context.fetch(fetchRequest)
+      } catch {
+        return []
+      }
+    print("loadLocations...")
+  } */
+  
+  private func lataaPaikkaObjekti() {
+    for location in locationList {
+      let locationObject = Location(context: CoreDataStack.context)
+      locationObject.timestamp = location.timestamp
+      locationObject.latitude = location.coordinate.latitude
+      locationObject.longitude = location.coordinate.longitude
+      print("Paikkaobjekti ladattu ...")
+    }
+  }
+  
   private func loadMap() {
     guard
       let locations = run?.locations,
@@ -168,8 +237,8 @@ class SavedTrailsDetailsVC: UIViewController, UINavigationControllerDelegate {
     
     mapView.setRegion(region, animated: true)
     mapView.addOverlays(polyLine())
-    //mapView.addAnnotations(annotations())
-    print("loadMap loaded")
+    mapView.addAnnotations(annotations())
+    print("loadMap loaded successfully")
   }
   
   private func segmentColor(speed: Double, midSpeed: Double, slowestSpeed: Double, fastestSpeed: Double) -> UIColor {
@@ -204,14 +273,41 @@ class SavedTrailsDetailsVC: UIViewController, UINavigationControllerDelegate {
     return UIColor(red: red, green: green, blue: blue, alpha: 1)
   }
   
+  private func annotations() -> [BadgeAnnotation] {
+    var annotations: [BadgeAnnotation] = []
+    let badgesEarned = Badge.allBadges.filter { $0.distance < run.distance }
+    var badgeIterator = badgesEarned.makeIterator()
+    var nextBadge = badgeIterator.next()
+    let locations = run.locations?.array as! [Location]
+    var distance = 0.0
+    
+    for (first, second) in zip(locations, locations.dropFirst()) {
+      guard let badge = nextBadge else { break }
+      let start = CLLocation(latitude: first.latitude, longitude: first.longitude)
+      let end = CLLocation(latitude: second.latitude, longitude: second.longitude)
+      distance += end.distance(from: start)
+      if distance >= badge.distance {
+        let badgeAnnotation = BadgeAnnotation(imageName: badge.imageName)
+        badgeAnnotation.coordinate = end.coordinate
+        badgeAnnotation.title = badge.name
+        badgeAnnotation.subtitle = FormatDisplay.distance(badge.distance)
+        annotations.append(badgeAnnotation)
+        nextBadge = badgeIterator.next()
+      }
+    }
+    
+    return annotations
+  }
+  
   
   // MARK: UI
-  
   fileprivate func setupUI() {
       view.addSubview(dateLabel)
       view.addSubview(distanceLabel)
       view.addSubview(durationLabel)
       //view.addSubview(mapView)
+      view.addSubview(dateLabelLocation)
+      view.addSubview(latitudeLabel)
       
       dateLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
       dateLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
@@ -227,9 +323,37 @@ class SavedTrailsDetailsVC: UIViewController, UINavigationControllerDelegate {
       durationLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
       durationLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -640).isActive = true
     
+    dateLabelLocation.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+    dateLabelLocation.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+    dateLabelLocation.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+
+    latitudeLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 240).isActive = true
+    latitudeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+    latitudeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
   }
   
 }
+
+// MARK: Location Manager Delegate
+/*extension SavedTrailsDetailsVC: CLLocationManagerDelegate {
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    for newLocation in locations {
+      let howRecent = newLocation.timestamp.timeIntervalSinceNow
+      guard newLocation.horizontalAccuracy < 20 && abs(howRecent) < 10 else { continue }
+      
+      if let lastLocation = locationList.last {
+        let delta = newLocation.distance(from: lastLocation)
+        distance = distance + Measurement(value: delta, unit: UnitLength.meters)
+        let coordinates = [lastLocation.coordinate, newLocation.coordinate]
+        mapView.addOverlay(MKPolyline(coordinates: coordinates, count: 2))
+        let region = MKCoordinateRegion.init(center: newLocation.coordinate, latitudinalMeters: 500, longitudinalMeters: 500)
+        mapView.setRegion(region, animated: true)
+      }
+      
+      locationList.append(newLocation)
+    }
+  }
+}*/
 
 // MARK: Map View Delegate
 extension SavedTrailsDetailsVC: MKMapViewDelegate {
@@ -243,7 +367,7 @@ extension SavedTrailsDetailsVC: MKMapViewDelegate {
     return renderer
   }
   
- /* func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+ func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
     guard let annotation = annotation as? BadgeAnnotation else { return nil }
     let reuseID = "checkpoint"
     var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseID)
@@ -260,6 +384,6 @@ extension SavedTrailsDetailsVC: MKMapViewDelegate {
     annotationView?.leftCalloutAccessoryView = badgeImageView
     
     return annotationView
-  } */
+  }
   
 }
